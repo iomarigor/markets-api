0f07212c8e407a8f42517beb943f68d90b1c3b5f
Iomar Igor Alegre Barrera <42124277+iomarigor@users.noreply.github.com>
2023-05-19 16:50:46 -0500

    first commit

---
+++ my-project/app/Models/Persons.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Persons extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'Persons';
+}
---
+++ my-project/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/', function (Request $request) {
+    phpinfo();
+});
+Route::get('/persons', function (Request $request) {
+    return json_encode(DB::table('Persons')->get());
+});
